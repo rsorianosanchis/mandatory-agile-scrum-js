c9bcd5ec5d344a47158f3aeccbfb2a66798037c6
rsoriano <rsorianosanchis@gmail.com>
2020-01-02 10:43:29 +0100

    grund av 2 comoponents, <MatchLista/> och <NewGame/>

--- chess-app/src/components/NewGame.js
+++ chess-app/src/components/NewGame.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 
-export default class NewGame extends Component {
+const initialState = {
+  id: '',
+  all_moves: [],
+  spelare1: '',
+  spelare2: '',
+  spelare1Color: '',
+  spelare2Color: '',
+  turn_to_move: 'white'
+};
+
+class NewGame extends Component {
   render() {
     return <div></div>;
   }
 }
+
+export default NewGame;
