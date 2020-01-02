89cdc422b10f99a1b7136a204246da78abec9eed
rsoriano <rsorianosanchis@gmail.com>
2020-01-02 11:37:50 +0100

    håller på <MatchLista>

--- chess-app/src/components/NewGame.js
+++ chess-app/src/components/NewGame.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 
 const initialState = {
-  id: '',
-  all_moves: [],
-  spelare1: '',
-  spelare2: '',
-  spelare1Color: '',
-  spelare2Color: '',
-  turn_to_move: 'white'
+  game: {
+    id: '',
+    all_moves: [],
+    spelare1: '',
+    spelare2: '',
+    spelare1Color: '',
+    spelare2Color: '',
+    turn_to_move: 'white'
+  }
 };
 
 class NewGame extends Component {
+  state = { ...initialState };
+
+  handleChange = e => {
+    console.log(e.target);
+    console.log(e.target.name + ':' + e.target.value);
+    this.setState({
+      cita: { ...this.state.game, [e.target.name]: e.target.value }
+    });
+  };
+
+  handleSubmit = e => {};
+
   render() {
-    return <div></div>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className='form-group row'>
+          <label className=''>Spelare 1</label>
+          <div className=''>
+            <input
+              type='text'
+              className='form-control'
+              placeholder='Namn Spelare 1'
+              name='spelare1'
+              value={this.state.game.spelare1}
+              onChange={this.handleChange}
+            />
+          </div>
+        </div>
+        {/*fin del impiut */}
+      </form>
+    );
   }
 }
 
