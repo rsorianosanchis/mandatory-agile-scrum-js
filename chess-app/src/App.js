65a3d9a4140423cd314c0a6ec7f7fab3ae96cb02
hesalk <42519593+hesalk@users.noreply.github.com>
2020-01-09 21:41:02 +0100

    added anrop to the server

--- chess-app/src/App.js
+++ chess-app/src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const axios = require('axios');
+
+let obj = {
+  spelare1 : "hesham",
+  spelare4 : "Ric",
+}
+
+axios.post("http://localhost:4000/api/seeks",obj)
+.then(res=>{console.log(res)})
+
 
 function App() {
   return (
--- chess-app/src/server.js
+++ chess-app/src/server.js
@@ -1,15 +1,43 @@
 var express = require('express')
 var app = express()
+var fs = require('fs');
+
+let matchlista = require('./lista.json')
+
 
 app.use(express.json())
 let port = 4000
 app.get('/',(req,res)=>{
+    console.log("get /")
     console.log(req.query)
     res.send("Welcome to the server");
+})
+
+//creating a new match 
+app.post("/api/seeks",(req,res)=>{
+    let obj = req.body
+    let arr = matchlista.list
+    console.log(obj)
+    console.log(Object.keys(obj).length)
+    console.log("api seeks")
+    if(Object.keys(obj).length > 2){
+        res.status(400).send("fel data").end()
+        return
+    }
+    arr.push(obj)
+    let makenew = {
+        "list":arr
+    }
+    let data = JSON.stringify(makenew)
+    fs.writeFile('./lista.json',data,(err) => {
+        if (err) throw err;
+        
+      })
+    res.send("dd")
 })
 
 
 app.listen(port, () => console.log(`Server runningg on port 4000!`))
 app.listen(() => {
-    console.log("Socket listening on 4000");
+    console.log("");
   })
