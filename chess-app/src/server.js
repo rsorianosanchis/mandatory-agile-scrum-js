5c29e6c0bdc3dc96428258fd03786b5f72dcf5ff
hesalk <42519593+hesalk@users.noreply.github.com>
2020-01-09 16:40:43 +0100

    added the basice server listner

--- chess-app/src/server.js
+++ chess-app/src/server.js
@@ -1,1 +1,15 @@
-ls
+var express = require('express')
+var app = express()
+
+app.use(express.json())
+let port = 4000
+app.get('/',(req,res)=>{
+    console.log(req.query)
+    res.send("Welcome to the server");
+})
+
+
+app.listen(port, () => console.log(`Server runningg on port 4000!`))
+app.listen(() => {
+    console.log("Socket listening on 4000");
+  })
