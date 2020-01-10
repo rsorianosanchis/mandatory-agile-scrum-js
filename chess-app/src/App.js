d5102899bfe92e6428853828bb806503b9ec3aee
rsoriano <rsorianosanchis@gmail.com>
2020-01-10 16:28:34 +0100

    uppdatera efter PR 2020/01/10

--- chess-app/src/App.js
+++ chess-app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 //import './App.css';
 import './bootstrap.min.css';
 import MatchLista from './components/MatchLista';
+const axios = require('axios');
 
 export default class App extends Component {
   state = { allGames: [] };
@@ -11,19 +12,14 @@ export default class App extends Component {
   }
 
   componentDidUpdate() {
-    // if något händer vi tar alGames från localstorage
     const memmory = localStorage.getItem('games');
     this.setState({ allGames: JSON.parse(memmory) });
   }
 
-  // GET data function från server, det körs i comopnentDidMunt
   getGames = async () => {
-    const axios = require('axios');
     try {
       const response = await axios.get('/api/seeks');
-      console.log(response);
       this.setState({ allGames: response.allGames });
-      // här vi gör en copy i local storage
       localStorage.setItem('games', JSON.stringify(this.state.allGames));
     } catch (error) {
       console.error(error);
