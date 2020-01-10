b46f1bd465c80995c0b475e17ce09ee4fd33a702
rsoriano <rsorianosanchis@gmail.com>
2020-01-10 11:29:45 +0100

    2020/01/10--11:29

--- chess-app/src/__test__/components/MatchLista.test.js
+++ chess-app/src/__test__/components/MatchLista.test.js
@@ -3,8 +3,8 @@ import { mount } from 'enzyme-adapter-react-16';
 import MatchLista from '../../components/MatchLista';
 
 describe('<MatchLista />', () => {
+  const matchlista = mount(<MatchLista />);
   test('test mount component MatchLista', () => {
-    const matchlista = mount(<MatchLista />);
     // vi testa om component är monterad i variable matchlista på riktig.
     expect(matchlista.length).toEqual(1);
   });
@@ -13,5 +13,3 @@ describe('<MatchLista />', () => {
     expect(matchlista.find('.lista').text()).toEqual('List of Games');
   });
 });
-
-//c:/Users/Ricardo L Soriano/EC_UTBILDNING/period_1_varen_2020/labb/mandatory-agile-scrum-js/chess-appnode_modules/@types/enzyme/index"
