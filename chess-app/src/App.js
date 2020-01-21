638f7dc459ad1b8863337eb61d57430eadcd89a4
rsorianosanchis <43029933+rsorianosanchis@users.noreply.github.com>
2020-01-21 17:05:04 +0100

    Revert "Feature/task20200102013"

--- chess-app/src/App.js
+++ chess-app/src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import './App.css';
+//import './App.css';
 import './bootstrap.min.css';
 import MatchLista from './components/MatchLista';
-import GameBoard from './components/gameBoard'
+import NewGame from './components/NewGame';
+
 export default class App extends Component {
+
+
   render() {
     return (
       <div className='container'>
-        <h4 className='title'>PLAY CHESS</h4>
-        <Router>
-          <div className="App">
-            <Route exact path="/" component={MatchLista} />
-            <Route path="/:gameId" component={GameBoard} />
-          </div>
-        </Router>
+        <div className=''>
+          <h4 className='title'>Chess project</h4>
+          <NewGame />
+          <MatchLista />
+
+        </div>
       </div>
     );
   }
--- chess-app/src/server.js
+++ chess-app/src/server.js
@@ -8,29 +8,22 @@ let matchlista = require('./lista.json')
 app.use(express.json())
 let port = 4000
 app.get('/',(req,res)=>{
-    console.log("user getting data")
+    console.log("get /")
+    console.log(req.query)
     res.send("Welcome to the server");
 })
 
 //creating a new match 
 app.post("/api/seeks",(req,res)=>{
-    console.log("creating match")
     let obj = req.body
     let arr = matchlista.list
-    let id = obj.id
-    function hitta (n) {
-        return n.id === id
-    }
-    if(arr.find(hitta)){
-        console.log("user sent wrong data a response to change sending")
-        res.status(400).send("ID already exist pleas change it").end()
-        return
-    }
-    if(Object.keys(obj).length < 2){
-        console.log("user entered small or empty object status 400 sent")
+    console.log(obj)
+    console.log(Object.keys(obj).length)
+    console.log("api seeks")
+/*     if(Object.keys(obj).length > 2){
         res.status(400).send("fel data").end()
         return
-    }
+    } */
     arr.push(obj)
     let makenew = {
         "list":arr
@@ -39,68 +32,36 @@ app.post("/api/seeks",(req,res)=>{
     fs.writeFile('./lista.json',data,(err) => {
         if (err) throw err;
         
-    })
-      res.status(201).send("matchen skapat").end()
+      })
+    res.send("Datab sparat to the server")
 })
 
 //get all matches 
 app.get("/api/seeks",(req,res)=>{
     let arr = matchlista.list
-    console.log("user asking for a full list of data")
-    res.status(200).send(arr).end()
+    console.log(arr)
+    res.send(arr)
 })
 
-//get en match
+//acceptera en match
 
 app.post("/api/seeks/:id",(req,res)=>{
     let arr = matchlista.list
     let id =  parseInt(req.params.id)
-    console.log("user getting a match data with id " + id)
+    console.log(id)
+    console.log("post")
     function hitta (n) {
         return n.id === id
     }
-    
     let result = arr.find(hitta)
-    if(arr.find(hitta)){
-        console.log("the match data is sending back to the user")
-        res.status(200).send(result).end()
-    }else{
-        console.log("the id was wrong")
-        res.status(204).send("there is no match with this id").end()
-    }
+    console.log(result)
+    res.send(result)
 })
 
 //update a match 
-app.put("/api/seeks/:id",(req,res)=>{
-    let arr = matchlista.list
-    let obj = req.body
-    let id = parseInt(req.params.id)
-    console.log("user is updateing a match med id "+ id)
-    function hitta (n) {
-        return n.id === id
-    }
-    let result = arr.find(hitta)
-    if(arr.find(hitta)){
-        console.log("servern hittade objected och den uppdateras")
-        let annat = arr.filter(function(n){
-            return n !== result
-        })
-        annat.push(obj)
-        let makenew = {
-        "list":annat
-    }
-        let data = JSON.stringify(makenew)
-        fs.writeFile('./lista.json',data,(err) => {
-        if (err) throw err;
-      })
-      res.status(200).send(result).end()
-    }else{
-        console.log("the id was wrong and user is getting fel data response")
-        res.status(400).send("there is no object for this id").end()
-    }
-})
+
 
 app.listen(port, () => console.log(`Server runningg on port 4000!`))
 app.listen(() => {
     console.log("");
-})
+  })
