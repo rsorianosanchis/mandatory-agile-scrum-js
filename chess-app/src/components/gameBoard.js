a95e0f6dab0d9e5bef86d9dc5202d4dba537a090
ibrahim yolbir <ibrahimyolbirr@gmail.com>
2020-01-23 13:28:15 +0100

    added game history

--- chess-app/src/components/gameBoard.js
+++ chess-app/src/components/gameBoard.js
@@ -145,11 +145,15 @@ export default class ChessImpl extends Component {
 
             });
     }
-resetGame = (e) => {
+    resetGame = (e) => {
         this.game.reset();
 
         this.setState(state => {
             state.fen = "start"
+            return state
+        })
+        this.setState(state => {
+            state.history = []
             return state
         })
         this.setState(state => {
@@ -160,13 +164,13 @@ resetGame = (e) => {
         const list = this.state.fenBoard;
 
         axios.put(`http://localhost:4000/api/seeks/${this.state.gameId}`, list)
-          .then((response) => {
-          // console.log(response);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }
+            .then((response) => {
+                // console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
 
     onMouseOverSquare = square => {
@@ -249,9 +253,8 @@ resetGame = (e) => {
                             "color": "black"
                         }}
                         to={"/"}> <i className="far fa-arrow-alt-circle-left"></i>  LOBBY </Link>
-                         <button onClick={this.resetGame}>Restart</button>
                 </header>
-                
+
                 <div style={{
                     "display": "flex",
                     "justifyContent": "space-around"
@@ -276,6 +279,24 @@ resetGame = (e) => {
                     />
                     <ChessHistory moveHistory={this.state.history} />
                 </div>
+                <footer 
+                style={{
+                    "position": "absolute",
+                    "bottom": 145,
+                    "textAlign": "center",
+                    "width": "100%"
+                }}
+                >
+                    <button onClick={this.resetGame}
+                    style={{
+                        "backgroundColor": "#478fc8",
+                        "border": "none",
+                        "color": "white",
+                        "padding": 15,
+                        "borderRadius": "27px 1px"
+                    }}
+                    >Restart Game</button>
+                </footer>
             </>
         )
     }
@@ -288,7 +309,7 @@ function ChessHistory(props) {
             return (
                 <div style={{
                     "display": "flex",
-                    "justifyContent":"space-around",
+                    "justifyContent": "space-around",
                     "fontWeight": "bold"
                 }} >
                     <span>White:</span>
@@ -301,7 +322,7 @@ function ChessHistory(props) {
             return (
                 <div style={{
                     "display": "flex",
-                    "justifyContent":"space-around"
+                    "justifyContent": "space-around"
                 }} >
                     <span>Black:</span>
                     <span>{move.from}</span>
@@ -322,13 +343,13 @@ function ChessHistory(props) {
         }}>
             <div style={{
                 "display": "flex",
-                "justifyContent":"space-around"
+                "justifyContent": "space-around"
             }} >
-                    <span>Color</span>
-                    <span>From</span>
-                    <span>To</span>
+                <span>Color</span>
+                <span>From</span>
+                <span>To</span>
             </div>
-                {moveHistory}
+            {moveHistory}
         </div>
     );
 }
