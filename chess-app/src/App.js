cb822d76d5eb89db85fbdb5123cc52ea04c540c4
Hesham Alkurdi <alkurdi.hesham@gmail.com>
2020-01-23 13:50:49 +0100

    added appa

--- chess-app/src/App.js
+++ chess-app/src/App.js
@@ -1,20 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
-<<<<<<< HEAD
-const axios = require('axios');
-
-let obj =  {
-  "id": 223,
-  "players": {
-      "Black": "ibo",
-      "White": "jhon"
-  },
-  "chessmans": "rnbqkbnr/1ppppp1p/6p1/p7/7P/2PP4/PP2PPP1/RNBQKBNR b KQkq h3 0 3",
-  "owner": "ibo",
-  "test": "3",
-  "test3":"ssss"
-=======
 import './bootstrap.min.css';
 import MatchLista from './components/MatchLista';
 import GameBoard from './components/gameBoard'
@@ -32,5 +18,4 @@ export default class App extends Component {
       </div>
     );
   }
->>>>>>> 21cc79266c0b1b58f1ac8457f811cf0f13630edf
 }
